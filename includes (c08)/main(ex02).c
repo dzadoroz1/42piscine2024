2b06390fb90fd1c16ffab8ce83184dc1acbf6259
dzadoroz1 <dandriq@icloud.com>
2024-10-08 10:03:31 +0200

    Add files via upload

---
+++ includes (c08)/main(ex02).c
@@ -0,0 +1,16 @@
+#include "ft_abs.h"
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    int b;
+    int c;
+
+    a = -5;
+    b = 5;
+    c = 0;
+    printf("%d\n", ABS(a));
+    printf("%d\n", ABS(b));
+    printf("%d\n", ABS(c));
+}
